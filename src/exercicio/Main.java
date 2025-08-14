d19d00505191b84867662d144b9bd0c3e8d3c6e0
Paulo Eduardo Rabelo De Miranda <0040482321037@americana.fatec.br>
2025-08-13 22:07:53 -0300

    Sálario fesso

--- src/exercicio/Main.java
+++ src/exercicio/Main.java
@@ -3,11 +3,12 @@ package exercicio;
 public class Main {
  
     public static void main(String[] args) {
-        Professor professor1 = new Professor("Lucas", "POO");
-        professor1.ministrou(60);
-        professor1.ExibirDados();
+        Professor professor1 = new Professor("Lucas", "POO", 350);
+        professor1.ministrou(40);
+        professor1.exibirDados();
         
         professor1.novoMes();
-        professor1.ExibirDados();
+        professor1.exibirDados();
+
     }
 }
--- src/exercicio/Professor.java
+++ src/exercicio/Professor.java
@@ -4,6 +4,7 @@ public class Professor {
     private String disciplina;
     private String nome;
     private int aulasMinistradas;
+    private double salarioPorAula;
 
     public String getDisciplina() {
         return disciplina;
@@ -20,10 +21,19 @@ public class Professor {
     public void setNome(String nome) {
         this.nome = nome;
     }
+
+    public double getSalarioPorAula() {
+        return salarioPorAula;
+    }
+
+    public void setSalarioPorAula(double salarioPorAula) {
+        this.salarioPorAula = salarioPorAula;
+    }
     
-    public Professor(String nome, String disciplina){
+    public Professor(String nome, String disciplina, double salarioPorAula){
       if(!nome.isEmpty()) this.nome = nome;
       if(!disciplina.isEmpty()) this.disciplina = disciplina;
+      if(salarioPorAula > 0) this.salarioPorAula = salarioPorAula;
       aulasMinistradas = 0;
     }
     
@@ -35,9 +45,14 @@ public class Professor {
       aulasMinistradas = 0;
     }
     
-    public void ExibirDados(){
+    public double calcSalario(){
+        return salarioPorAula * aulasMinistradas;
+    }
+    
+    public void exibirDados(){
         System.out.println("Nome do professor: " + nome);
         System.out.println("Disciplina do professor: " + disciplina);
         System.out.println("Aulas Ministradas pelo professor: " + aulasMinistradas);
+        System.out.println("Salário do professor: " + calcSalario());
     }
 }
